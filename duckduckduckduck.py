4318dc81e44873a6601c5499828e5d79fc24da8c
Tofu <jocetseinsf@gmail.com>
2025-10-15 08:35:21 -0700

    toes

---
+++ duckduckduckduck.py
@@ -0,0 +1,14 @@
+import time
+print("tofu soup - fried chicken")
+x = input ()
+x = 2
+if x == 2: 
+    print("tofu is a simp")
+elif x == 4:
+    print("balding teachers")
+for x in range (0, 10):
+    print("ryan is bald" + str(x))
+    time.sleep(6)
+
+elif x==9
+

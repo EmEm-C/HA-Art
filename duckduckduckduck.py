21e4ffc4f6c87bceaf195217322f21cd6e068da8
Tofu <jocetseinsf@gmail.com>
2025-10-15 13:17:00 -0700

    toes

--- duckduckduckduck.py
+++ duckduckduckduck.py
@@ -1,16 +1,19 @@
 import time
+def tofu_soup():
+    
 print("tofu soup - said fried chicke")
-x = input ()
 x = 2
 if x == 2: 
-    print("tofu is a simp")
+    print("tofu is a simp :D")
 elif x == 4:
     print("balding teachers")
 for x in range (0, 10):
     print("ryan is bald" + str(x))
     time.sleep(6)
 
-elif x==9:
+if x == 9:
     print("my little poney")
- elif x== 10:
-    print("toes ")
+elif x == 10:
+    print("baldie's basic")
+try:
+    print("Balding owen")
